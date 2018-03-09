b625f6bc3ed84e39267bdb60415061addea93151
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-03-09 17:08:47 +0100

    off by one AIC?

--- src/aic.js
+++ src/aic.js
@@ -4,7 +4,7 @@ class AIC {
     static compute(clusters, data, cs) {
         var ll = BIC._loglikelihood(clusters, data, cs);
         var dims = data[0].length;
-        var freeParams = cs.length * (dims);
+        var freeParams = cs.length * (dims + 1);
         return -(2 * freeParams - 2 * ll);
     }
 }
