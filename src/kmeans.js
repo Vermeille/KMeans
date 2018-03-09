dad101e46e039168b0c15f98ccdf442c6bf3e3d4
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-03-09 14:36:33 +0100

    var => let

--- src/kmeans.js
+++ src/kmeans.js
@@ -1,6 +1,6 @@
-var Vector = require('vec');
-var BIC = require('./bic.js');
-var AIC = require('./aic.js');
+let Vector = require('vec');
+let BIC = require('./bic.js');
+let AIC = require('./aic.js');
 
 class KMeans {
     constructor(cs_or_k, data, loss='bic', guardMax=10000) {
@@ -10,7 +10,7 @@ class KMeans {
         } else {
             this.k = cs_or_k;
             this.centroids = [];
-            for (var i = 0; i < this.k; ++i) {
+            for (let i = 0; i < this.k; ++i) {
                 this.centroids.push(data[Math.floor(Math.random() * data.length)]);
             }
         }
@@ -20,9 +20,9 @@ class KMeans {
     }
 
     assignAll(clusters, points, cs) {
-        var converged = true;
-        for (var i = 0; i < points.length; ++i) {
-            var newc = Vector.closest(points[i], cs);
+        let converged = true;
+        for (let i = 0; i < points.length; ++i) {
+            let newc = Vector.closest(points[i], cs);
             converged = converged && newc == clusters[i];
             clusters[i] = newc;
         }
@@ -30,34 +30,34 @@ class KMeans {
     }
 
     countClusters(clusters, c) {
-        var n = 0;
-        for (var i = 0; i < clusters.length; ++i) {
+        let n = 0;
+        for (let i = 0; i < clusters.length; ++i) {
             n += clusters[i] == c;
         }
         return n;
     }
 
     computeCentroids(clusters, points, cs) {
-        var nb = cs.map((_, i) => this.countClusters(clusters, i));
+        let nb = cs.map((_, i) => this.countClusters(clusters, i));
         nb.forEach((n, i) => {
             if (n !== 0) {
                 return;
             }
-            var takeFrom = nb.findIndex(m => m > 1);
-            var taken = clusters.findIndex(x => x === takeFrom);
+            let takeFrom = nb.findIndex(m => m > 1);
+            let taken = clusters.findIndex(x => x === takeFrom);
             cs[i] = points[taken];
             clusters[taken] = i;
             --nb[takeFrom];
             ++nb[i];
         });
 
-        for (var i = 0; i < cs.length; ++i) {
+        for (let i = 0; i < cs.length; ++i) {
             cs[i] = Vector.zero(points[0].length);
         }
-        for (var i = 0; i < points.length; ++i) {
+        for (let i = 0; i < points.length; ++i) {
             Vector.add(cs[clusters[i]], points[i]);
         }
-        for (var i = 0; i < cs.length; ++i) {
+        for (let i = 0; i < cs.length; ++i) {
             Vector.div(cs[i], this.countClusters(clusters, i));
         }
     }
@@ -68,13 +68,13 @@ class KMeans {
         } else if (this.loss === 'aic') {
             return AIC.compute(clusters, points, cs);
         } else {
-            var s = Vector.zero(cs.length);
-            var count = Vector.zero(cs.length);
-            for (var i = 0; i < points.length; ++i) {
+            let s = Vector.zero(cs.length);
+            let count = Vector.zero(cs.length);
+            for (let i = 0; i < points.length; ++i) {
                 s[clusters[i]] += Vector.distance(points[i], cs[clusters[i]]);
                 ++count[clusters[i]];
             }
-            for (var i = 0; i < count.length; ++i) {
+            for (let i = 0; i < count.length; ++i) {
                 s[i] /= count[i];
             }
             return -Vector.sum(s);
@@ -82,13 +82,13 @@ class KMeans {
     }
 
     start() {
-        var data = this.data;
-        var k = this.k;
-        var kmeans = {};
+        let data = this.data;
+        let k = this.k;
+        let kmeans = {};
         kmeans.k = k;
         kmeans.cluster = [];
         kmeans.centroids = this.centroids;
-        for (var i = 0; i < data.length; ++i) {
+        for (let i = 0; i < data.length; ++i) {
             kmeans.cluster.push(0);
         }
 
@@ -106,7 +106,7 @@ class KMeans {
         }
         kmeans.score = this.score(kmeans.cluster, data, kmeans.centroids);
         kmeans.closest = [];
-        for (var i = 0; i < k; ++i) {
+        for (let i = 0; i < k; ++i) {
             kmeans.closest.push(Vector.closest(kmeans.centroids[i], data));
         }
         return kmeans;
@@ -137,7 +137,12 @@ class KMeansOptimizer {
     }
 
     step_try() {
-        var ktry = (new KMeans(this.k, this.data, this.loss)).start();
+        let ktry;
+        try {
+            ktry = (new KMeans(this.k, this.data, this.loss)).start();
+        } catch (e) {
+            ktry = { score: parseFloat('-Infinity') };
+        }
         if (this.t == 0) {
             this.best_try = ktry;
         } else if (ktry.score > this.best_try.score) {
@@ -170,10 +175,10 @@ class KMeansOptimizer {
                     km.score > best.score ? km : best
             );
         } else {
-            var best_k = 1;
-            var best_d2 = 0;
-            for (var k = 3; k < this.maxk; ++k) {
-                var d2 = this.kmeans[k - 1].score + this.kmeans[k + 1].score
+            let best_k = 1;
+            let best_d2 = 0;
+            for (let k = 3; k < this.maxk; ++k) {
+                let d2 = this.kmeans[k - 1].score + this.kmeans[k + 1].score
                     - 2 * this.kmeans[k].score;
                 if (Math.abs(d2) > best_d2) {
                     best_k = k;
